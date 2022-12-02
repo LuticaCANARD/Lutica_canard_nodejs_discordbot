0c6b51f0379006681cfa12e82e6f8b66b45f5d16
LuticaCANARD <80238084+LuticaCANARD@users.noreply.github.com>
2022-12-02 20:25:32 +0900

    maininiot

--- bot.js
+++ bot.js
@@ -1,4 +1,3 @@
-const fs = require('node:fs');
 const path = require('node:path');
 const { Collection,Client, GatewayIntentBits,SlashCommandBuilder,Events, REST, Routes } = require('discord.js');
 const client = new Client({ 
@@ -9,8 +8,9 @@ const client = new Client({
       GatewayIntentBits.MessageContent,
       GatewayIntentBits.GuildMembers,
     ] });
-    /*
-client.commands = new Collection();
+const fs = require('fs');
+client.commands = new Collection() 
+// 명령어 캐시 컬렉션을 클라이언트 내에 선언한다. 해당 방법으로 명령어 파일 내에서도 client.commands로 다른 명령어들에 접근할수 있다.
 
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
@@ -26,5 +26,25 @@ for (const file of commandFiles) {
 	}
 }
 
-*/
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+
+client.on('ready',()=>{console.log('ready!')})
+
+// Log in to Discord with your client's token
 client.login(process.env.TOKEN);// 토큰 수정
--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,4 @@
-/*const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -7,4 +7,4 @@ module.exports = {
 	async execute(interaction) {
 		await interaction.reply('Pong!');
 	},
-};*/
+};
