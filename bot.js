5258126ccef27b673de55bfbc39ce88c5afca87c
LuticaCANARD <80238084+LuticaCANARD@users.noreply.github.com>
2022-12-02 19:00:34 +0900

    olk

--- bot.js
+++ bot.js
@@ -1,14 +1,30 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const fs = require('node:fs');
+const path = require('node:path');
+const { Collection,Client, GatewayIntentBits,SlashCommandBuilder,Events, REST, Routes } = require('discord.js');
+const client = new Client({ 
+    intents: 
+    [ 
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.MessageContent,
+      GatewayIntentBits.GuildMembers,
+    ] });
+    /*
+client.commands = new Collection();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-client.on('message', msg => {
-  if (msg.content === '!ping') {
-    msg.reply('Pong!');
-  }
-});
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
 
+*/
 client.login(process.env.TOKEN);// 토큰 수정
