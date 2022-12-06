a7178914304bf02cbede04b33405de626c0fc8ee
LuticaCANARD <80238084+LuticaCANARD@users.noreply.github.com>
2022-12-06 20:36:06 +0900

    fixed comms

--- commands/showgithub.js
+++ commands/showgithub.js
@@ -4,11 +4,11 @@ var langutil = require('./util/lang_util/langutil.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('show github')
+		.setName('show_github')
 		.setDescription('show github repogistry'),
 	async execute(interaction) {
         let langclass = new langutil(interaction.locale)
-        let return_string = langclass.replaceStringVal(langclass.getLangStr(2),['https://github.com/LuticaCANARD/Lutica_canard_nodejs_discordbot']);
+        let return_string = langclass.replaceStringVal(langclass.getLangStr(2),['https://github.com/LuticaCANARD/Lutica_canard_nodejs_discordbot ']);
 		await interaction.reply(return_string);
 	},
 };
